fc5e856728a69ef1c52d0dbbaa6d7696100d4470
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-04-09 10:12:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,50 @@
 import os 
 
-print ("""sabor express
-       """)
+restalrante = ['pizza','xp'] # lista de nomes de restalrante
 
-print ('1. cadastrar restalrante')
-print ('2. listar restalrante')
-print ('3. ativar restalrante')
-print ('4. sair')
-#https://fsymbols.com/pt/letras/
+def exibir_nome_do_programa():
+ print ("""sabor express
+ """)
+def exibir_opcoes():
+ print ('1. cadastrar restalrante')
+ print ('2. listar restalrante')
+ print ('3. ativar restalrante')
+ print ('4. sair')
+
+def finaliza_app():
+   exibir_subtitulo('finalizar app')
+
+def voltar_ao_menu_principal():
+   input('\n digite a tecla "enter" para voltar ao menu principal')
+   main()
+
+
 
 opcao_escolhida =int(input('escolha uma opcao: '))
 #print('voce escolheu a opcao' , opcao_escolhida)
 #print(f' voce escolheu a opcao {opcao_escolha}' )
 
-def finaliza_app():
+def exibir_subtitulo(texto):
     os.system('cls') #os.system('clear')
-    print('encerrando o programa\n')
+    print(texto)
+    print()
 
-if opcao_escolhida == 1:
-    print('cadastrar restalrante')
- elif opcao_escolhida == 2:
-    print(' listar restalrante')
-elif opcao_escolhida == 3:
-    print('ativar restalrante')
-else:
-    finaliza_app()
+def escolher_opcao():
+
+    if opcao_escolhida == 1:
+        print('cadastrar restalrante')
+    elif opcao_escolhida == 2:
+        print(' listar restalrante')
+    elif opcao_escolhida == 3:
+        print('ativar restalrante')
+    else:
+        finaliza_app()
+
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ =='_main_':
+   main()
