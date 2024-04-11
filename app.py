f10091e101c527552c11b9c8b916b24ade9069d9
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-04-11 08:45:00 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os 
 
-restalrante = ['pizza','xp'] # lista de nomes de restalrante
+restalrantes = ['pizza','xp'] # lista de nomes de restalrante
 
 def exibir_nome_do_programa():
  print ("""sabor express
@@ -18,33 +18,53 @@ def voltar_ao_menu_principal():
    input('\n digite a tecla "enter" para voltar ao menu principal')
    main()
 
-
-
-opcao_escolhida =int(input('escolha uma opcao: '))
-#print('voce escolheu a opcao' , opcao_escolhida)
-#print(f' voce escolheu a opcao {opcao_escolha}' )
+def opcao_invalida():
+    print('opcao_invalida!\n')
+    voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
     os.system('cls') #os.system('clear')
     print(texto)
     print()
 
+def cadastrar_novo_restalrante():
+   exibir_subtitulo('cadastro de novos restalrantes:')
+   nome_do_restalrante = input('digite o nome do novo restalrante')
+   restalrantes.append(nome_do_restalrante)
+   print(f'o restalrante{nome_do_restalrante}foi cadastrado com sucesso!')
+   voltar_ao_menu_principal()
+
+   def listar_restalrante():
+      exibir_subtitulo('listando os restalrantes')
+
+      for restalrante in restalrantes:
+         print(f'*{restalrante}')
+
+   voltar_ao_menu_principal()
+
 def escolher_opcao():
+   try:
+    opcao_escolhida =int(input('escolha uma opcao: '))
 
     if opcao_escolhida == 1:
-        print('cadastrar restalrante')
+       cadastrar_novo_restalrante()
     elif opcao_escolhida == 2:
-        print(' listar restalrante')
+       listar_restalrante()
     elif opcao_escolhida == 3:
         print('ativar restalrante')
+    elif opcao_escolhida== 4:
+       finaliza_app()
     else:
         finaliza_app()
+
+   except:
+    opcao_invalida()
 
 def main():
     os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
-    escolher_opcoes()
+    escolher_opcao()
 
 if __name__ =='_main_':
    main()
